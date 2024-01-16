8257a7bb86375ab3c1f026f15ab3766cb66cd5db
Match <match.all.tamirat@gmail.com>
2024-01-16 13:04:37 -0800

    JavaScript Expressions

--- myScript.js
+++ myScript.js
@@ -8,10 +8,11 @@ let T, F;
   T = 12;
   F = 3 ;
   document.getElementById ('opr').innerHTML = T + F ;
-
+  document.getElementById('xp').innerHTML = 5 * 10; //JavaScript Expressions
 document.getElementById("ops").innerHTML = (5 + 5) * 1;
 document.getElementById("mo").innerHTML = "This is the value of x:" + x ;
-document.getElementById("number").innerHTML = 10.5; ///  number
+document.getElementById("number").innerHTML = 10.5; //number
 document.getElementById("string").innerHTML = "John Doe"; //Strings
-document
-//JavaScript Operators end.
+document //JavaScript Operators end.
+
+document.getElementById('st').innerHTML ="Tamirat" + " " + "Teferi";
