d42ef020e8c1835fd748a176c47396943069777f
Match <match.all.tamirat@gmail.com>
2024-01-16 12:41:09 -0800

    JavaScript Operators

--- myScript.js
+++ myScript.js
@@ -2,7 +2,7 @@
 
 let x; //JavaScript Variables
 x = 6; //JavaScript Variables
-document.getElementById("ops").innerHTML = (5 + 4) * 1;
+document.getElementById("ops").innerHTML = (5 + 5) * 1;
 document.getElementById("mo").innerHTML = "This is the value of x:" + x ;
 document.getElementById("number").innerHTML = 10.5; ///  number
 document.getElementById("string").innerHTML = "John Doe"; //Strings
